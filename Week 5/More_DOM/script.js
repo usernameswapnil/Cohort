24a7715e430ff34c0ff168454ffd13ff6a639cc4
Swapnil Kumar Singh <swapnilkumarsingh54@gmail.com>
2025-02-17 23:31:29 +0530

    More DOM

---
+++ Week 5/More_DOM/script.js
@@ -0,0 +1,7 @@
+let div = document.querySelector("div");
+console.log(div);
+let value = div.getAttribute("id"); //tell
+console.log(value);
+
+let para = document.querySelector("p");
+console.log(para.setAttribute("class", "newroom"));
